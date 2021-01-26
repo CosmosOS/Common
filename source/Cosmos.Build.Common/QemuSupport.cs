5946592023ebddd27bf2ceed2106a9d553c06d72
Charles Betros <charles.betros@gmail.com>
2021-01-26 11:34:06 -0600

    Check environment for qemu path

--- source/Cosmos.Build.Common/QemuSupport.cs
+++ source/Cosmos.Build.Common/QemuSupport.cs
@@ -18,15 +18,9 @@ namespace Cosmos.Build.Common
         }
 
         /// <summary>Get a flag that tell whether Qemu is enabled on this system.</summary>
-        public static bool QemuEnabled
-        {
-            get
-            {
-                return (null != QemuExe);
-                //return false;
-            }
-        }
+        public static bool QemuEnabled => QemuExe != null && QemuExe.Exists;
 
+        //return false;
         /// <summary>Get a descriptor for the Qemu emulator program. The return value is a null reference if
         /// Qemu is not installed.</summary>
         public static FileInfo QemuExe
@@ -35,90 +29,32 @@ namespace Cosmos.Build.Common
             private set;
         }
 
-        /// <summary>Retrieve installation path for Qemu and initialize the <see cref="QemuExe"/> property.
-        /// Search is performed using the registry and rely on the shell command defined for the
-        /// QemuConfigFile.</summary>
+        /// <summary>Retrieve installation path for Qemu and initialize the <see cref="QemuExe"/> property.</summary>
         private static void FindQemuExe()
         {
-            // try
-            // {
-            //     using (var runCommandRegistryKey =
-            //         Registry.ClassesRoot.OpenSubKey(@"QemuConfigFile\shell\Run\command", false))
-            //     {
-            //         if (null == runCommandRegistryKey)
-            //         {
-            //             return;
-            //         }
+            string qemuEnvironment = Environment.GetEnvironmentVariable("QEMU");
+            string qemuPath = Path.GetDirectoryName(qemuEnvironment);
+            string qemuExe = Path.GetFileName(qemuEnvironment);
 
-            //         string commandLine = (string) runCommandRegistryKey.GetValue(null, null);
-            //         if (null != commandLine)
-            //         {
-            //             commandLine = commandLine.Trim();
-            //         }
+            if (!string.IsNullOrWhiteSpace(qemuExe) && File.Exists(qemuEnvironment))
+            {
+                QemuExe = new FileInfo(qemuEnvironment);
+                return;
+            }
 
-            //         if (string.IsNullOrEmpty(commandLine))
-            //         {
-            //             return;
-            //         }
-
-            //         // Now perform some parsing on command line to discover full exe path.
-            //         string candidateFilePath;
-            //         int commandLineLength = commandLine.Length;
-            //         if ('"' == commandLine[0])
-            //         {
-            //             // Seek for a non escaped double quote.
-            //             int lastDoubleQuoteIndex = 1;
-            //             for (; lastDoubleQuoteIndex < commandLineLength; lastDoubleQuoteIndex++)
-            //             {
-            //                 if ('"' != commandLine[lastDoubleQuoteIndex])
-            //                 {
-            //                     continue;
-            //                 }
-
-            //                 if ('\\' != commandLine[lastDoubleQuoteIndex - 1])
-            //                 {
-            //                     break;
-            //                 }
-            //             }
-
-            //             if (lastDoubleQuoteIndex >= commandLineLength)
-            //             {
-            //                 return;
-            //             }
-
-            //             candidateFilePath = commandLine.Substring(1, lastDoubleQuoteIndex - 1);
-            //         }
-            //         else
-            //         {
-            //             // Seek for first separator character.
-            //             int firstSeparatorIndex = 0;
-            //             for (; firstSeparatorIndex < commandLineLength; firstSeparatorIndex++)
-            //             {
-            //                 if (char.IsSeparator(commandLine[firstSeparatorIndex]))
-            //                 {
-            //                     break;
-            //                 }
-            //             }
-
-            //             if (firstSeparatorIndex >= commandLineLength)
-            //             {
-            //                 return;
-            //             }
-
-            //             candidateFilePath = commandLine.Substring(0, firstSeparatorIndex);
-            //         }
-
-            //         if (!File.Exists(candidateFilePath))
-            //         {
-            //             return;
-            //         }
-
-            //         QemuExe = new FileInfo(candidateFilePath);
-            //     }
-            // }
-            // catch
-            // {
-            // }
+            if (!string.IsNullOrWhiteSpace(qemuPath) && Directory.Exists(qemuPath))
+            {
+                var files = Directory.GetFiles(qemuPath, "*.exe");
+                foreach (var file in files)
+                {
+                    string fileName = Path.GetFileName(file);
+                    if (fileName.Equals("qemu.exe", StringComparison.OrdinalIgnoreCase) || fileName.Contains("86"))
+                    {
+                        QemuExe = new FileInfo(file);
+                        return;
+                    }
+                }
+            }
         }
     }
 }
