f6eee7c904a068e1a73627f40855d37a2423b7d1
TheFocusMan <sdshai2004@gmail.com>
2022-10-04 13:48:58 +0300

    Revert "Add Fast boost option"
    This reverts commit 576233dde9e37b0aa1604e056a95e55a88d91dde.

--- source/Cosmos.Build.Common/BuildPropertyNames.cs
+++ source/Cosmos.Build.Common/BuildPropertyNames.cs
@@ -33,7 +33,6 @@ namespace Cosmos.Build.Common
         public const string CompressionTypeString = "CompressionType";
         public const string IsoFileString = "ISOFile";
         public const string CompileVBEMultiboot = "CompileVBEMultiboot";
-        public const string EnableFastBoost = "EnableFastBoost";
         public const string VBEResolution = "VBEResolution";
         public const string ExtractMapFile = "ExtractMapFile";
         public const string QemuMemoryString = "QemuMemory";
